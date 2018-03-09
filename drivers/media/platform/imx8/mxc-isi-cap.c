4056aacdb2b893c6455c299881dbce662fac83c2
Guoniu.Zhou <guoniu.zhou@nxp.com>
2018-03-09 16:19:37 +0800

    MLK-17741-2: media: add ov5640 mipi driver
    Add driver for ov5640 mipi csi interface.
    
    Reviewed-by: Sandor.Yu <sandor.yu@nxp.com>
    Signed-off-by: Guoniu.Zhou <guoniu.zhou@nxp.com>
    (cherry picked from commit 860dab368735dcbf248da7b74163ce6048a57514)

--- drivers/media/platform/imx8/mxc-isi-cap.c
+++ drivers/media/platform/imx8/mxc-isi-cap.c
@@ -220,7 +220,8 @@ struct mxc_isi_fmt *mxc_isi_get_src_fmt(struct v4l2_subdev_format *sd_fmt)
 
 	/* two fmt RGB32 and YUV444 from pixellink */
 	if (sd_fmt->format.code == MEDIA_BUS_FMT_YUYV8_1X16 ||
-		sd_fmt->format.code == MEDIA_BUS_FMT_YVYU8_2X8)
+		sd_fmt->format.code == MEDIA_BUS_FMT_YVYU8_2X8 ||
+		sd_fmt->format.code == MEDIA_BUS_FMT_UYVY8_2X8)
 		index = 1;
 #ifdef CONFIG_IWG27M
         /* IWG27M: IMX230 Camera support customization */
