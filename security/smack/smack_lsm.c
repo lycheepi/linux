e396d6c854775bc6a4c4b254c1e22f025c43787b
Truong Van Huy <truongvanhuy1995@outlook.com>
2019-10-16 09:55:58 +0700

    Revert "Enable smack security"
    This reverts commit fc7e7e756f0d52ac24130c16bcf5dae9070f9192.

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -2337,16 +2337,8 @@ static int smack_sk_alloc_security(struct sock *sk, int family, gfp_t gfp_flags)
 	if (ssp == NULL)
 		return -ENOMEM;
 
-	/*
-	 * Sockets created by kernel threads receive web label.
-	 */
-	if (unlikely(current->flags & PF_KTHREAD)) {
-		ssp->smk_in = &smack_known_web;
-		ssp->smk_out = &smack_known_web;
-	} else {
-		ssp->smk_in = skp;
-		ssp->smk_out = skp;
-	}
+	ssp->smk_in = skp;
+	ssp->smk_out = skp;
 	ssp->smk_packet = NULL;
 
 	sk->sk_security = ssp;
